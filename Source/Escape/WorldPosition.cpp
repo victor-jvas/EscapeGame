cbb6d3a9ed7a6b716beda1d2362133359fc5a2a9
jVictor <victor.jvas1@gmail.com>
2021-04-21 16:41:55 -0300

    Initial commit

---
+++ Source/Escape/WorldPosition.cpp
@@ -0,0 +1,39 @@
+
+
+#include "WorldPosition.h"
+
+// Sets default values for this component's properties
+UWorldPosition::UWorldPosition()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWorldPosition::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const FString Name = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *Name);
+
+	const FString ObjectPosition = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *ObjectPosition);
+
+	
+	
+}
+
+
+// Called every frame
+void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
